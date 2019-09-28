c9a5adc5d7d812bba817f00656211042a4ac3473
Ha <phammanhha121095@gmail.com>
2019-09-28 10:38:34 +0700

    ++++
    ====

---
+++ ContactList/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  ContactList
+//
+//  Created by Phạm Mạnh Hà on 9/28/19.
+//  Copyright © 2019 Phạm Mạnh Hà. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       view.backgroundColor = .red
+        
+        
+    }
+
+
+}
